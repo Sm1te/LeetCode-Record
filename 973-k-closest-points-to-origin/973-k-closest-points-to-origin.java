fe3aefc375afd43b85b8569ae41cbd35242181f1
Robin Li <liyijiannice@outlook.com>
2022-06-24 15:00:24 -0700

    Time: 34 ms (79.92%), Space: 116.7 MB (57.45%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int k) {
+    	Arrays.sort(points, (a, b) -> 
+            (a[0] * a[0] + a[1] * a[1]) - (b[0] * b[0] + b[1] * b[1])
+        );
+        int[][] res = new int[k][2];
+        for(int i = 0; i < k; i++){
+            res[i] = points[i];
+        }
+        return res;
+    }
+}
