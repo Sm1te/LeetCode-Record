365e0b2e251cc13606855bcb24a0ae7515b73521
Robin Li <liyijiannice@outlook.com>
2022-06-30 19:44:17 -0500

    Time: 64 ms (5.49%), Space: 114.7 MB (5.03%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
         int[][] dp = new int[prices.length][2];
-        dp[0][0] = 0;
-        dp[0][1] = -prices[0];
+        int t0_0 = 0;
+        int t0_1 = -prices[0];
         
         for(int i = 1; i < prices.length; i++){
-            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
-            dp[i][1] = Math.max(dp[i - 1][1], -prices[i]);
+            t0_0 = Math.max(t0_0, t0_1 + prices[i]);
+            t0_1 = Math.max(t0_1, -prices[i]);
         }
         
-        return dp[prices.length - 1][0];
+        return t0_0;
     }
 }
