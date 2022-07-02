11bb48ba0c196594a8ba40c091bfa6548159f999
Robin Li <liyijiannice@outlook.com>
2022-07-02 00:52:48 -0500

    Time: 12 ms (63.66%), Space: 44.2 MB (58.41%) - LeetHub

---
+++ 224-basic-calculator/224-basic-calculator.java
@@ -0,0 +1,33 @@
+class Solution {
+    public static int calculate(String s) {
+        int len = s.length(), sign = 1, result = 0;
+        Stack<Integer> stack = new Stack<Integer>();
+        
+        for (int i = 0; i < len; i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                int sum = s.charAt(i) - '0';
+                while (i + 1 < len && Character.isDigit(s.charAt(i + 1))) {
+                    sum = sum * 10 + s.charAt(i + 1) - '0';
+                    i++;
+                }
+                // from left to right, we calculate before any parenthesis
+                result += sum * sign;
+            } else if (s.charAt(i) == '+')
+                sign = 1;
+            else if (s.charAt(i) == '-')
+                sign = -1;
+            else if (s.charAt(i) == '(') {
+                // we cannot use sign * result to store, cause when we deal with 1 - (3 + 5)
+                // that would be 8 - 1 = 7 instead of -7
+                stack.push(result);
+                stack.push(sign);
+                result = 0;
+                sign = 1;
+            } else if (s.charAt(i) == ')') {
+                result = result * stack.pop() + stack.pop();
+            }
+
+        }
+        return result;
+    }
+}
