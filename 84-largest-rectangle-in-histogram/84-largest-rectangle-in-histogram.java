6a2ccaa68b6441319b5e4d9ba8d78658608ed896
Robin Li <liyijiannice@outlook.com>
2022-07-03 02:24:32 -0500

    Time: 72 ms (74.52%), Space: 98.9 MB (18.22%) - LeetHub

---
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        
+        
+        Deque<Integer> mono_stack = new ArrayDeque();
+        for(int i = 0; i < n; i++){
+            while(!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]){
+                mono_stack.pop();
+            }
+            left[i] = mono_stack.isEmpty() ? -1 : mono_stack.peek();
+            mono_stack.push(i);
+        }
+        
+        mono_stack.clear();
+        
+        for(int i = n - 1; i >= 0; i--){
+            while(!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]){
+                mono_stack.pop();
+            }
+            right[i] = mono_stack.isEmpty() ? n : mono_stack.peek();
+            mono_stack.push(i);
+        }
+        
+        
+        int res = 0;
+        for(int i = 0; i < n; i++){
+            res = Math.max(res, (right[i] - left[i] - 1) * heights[i]);
+        }
+        
+        return res;
+    }
+}
