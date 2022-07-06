f80ea771ebbba6123e7305aa4fedbdf565819152
Robin Li <liyijiannice@outlook.com>
2022-07-06 17:01:20 -0500

    Time: 30 ms (49.25%), Space: 44.1 MB (81.79%) - LeetHub

--- 322-coin-change/322-coin-change.java
+++ 322-coin-change/322-coin-change.java
@@ -1,19 +1,20 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if(amount < 1)
+        if(amount == 0)
             return 0;
-        int max = amount + 1;
-        int[] dp = new int[max];
-        Arrays.fill(dp, max);
+        
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, 10001);
         dp[0] = 0;
+       
         for(int i = 1; i <= amount; i++){
             for(int coin : coins){
-                if(coin <= i){
-                    dp[i] = Math.min(dp[i - coin] + 1, dp[i]);
-                }
+                if(i - coin < 0)
+                    continue;
+                
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
             }
         }
-        return dp[amount] == max ? -1 : dp[amount];
+        return dp[amount] == 10001 ? -1 : dp[amount];
     }
-    
 }
