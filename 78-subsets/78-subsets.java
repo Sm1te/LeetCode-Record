86c0b16cbd01951fcaa8065a05d299c4b570914a
Robin Li <liyijiannice@outlook.com>
2022-07-29 18:56:40 -0500

    Time: 1 ms (89.28%), Space: 43.7 MB (24.65%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -1,21 +1,23 @@
 class Solution {
-    List<List<Integer>> res = new ArrayList();
-    LinkedList<Integer> list = new LinkedList();
-    
+    List<List<Integer>> res;
     public List<List<Integer>> subsets(int[] nums) {
-        backTrack(nums, 0);
+        res = new ArrayList();
+        backTrack(0, nums, new LinkedList());
         return res;
     }
     
-    public void backTrack(int[] nums, int startIndex){
-        if(list.size() <= nums.length){
-            res.add(new LinkedList(list));   
+    public void backTrack(int start, int[] nums, LinkedList<Integer> tmp){
+        if(tmp.size() > nums.length || start > nums.length)
+            return;
+        
+        res.add(new ArrayList(tmp));
+        
+        for(int i = start; i < nums.length; i++){
+            tmp.add(nums[i]);
+            backTrack(i + 1, nums, tmp);
+            tmp.removeLast();
         }
         
-        for(int i = startIndex; i < nums.length; i++){
-            list.add(nums[i]);
-            backTrack(nums, i + 1);
-            list.removeLast();
-        }  
+        
     }
 }
