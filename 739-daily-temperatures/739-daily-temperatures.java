ff822516df6dfcae2d5e9a01a042f0e19c47296b
Robin Li <liyijiannice@outlook.com>
2022-08-08 16:30:44 -0500

    Time: 46 ms (77.77%), Space: 134.9 MB (5.49%) - LeetHub

---
+++ 739-daily-temperatures/739-daily-temperatures.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] res = new int[n];
+
+        // 这里放元素索引，而不是元素
+        Deque<Integer> s = new ArrayDeque<>(); 
+        /* 单调栈模板 */
+        for (int i = n - 1; i >= 0; i--) {
+            while (!s.isEmpty() && temperatures[s.peek()] <= temperatures[i]) {
+                s.pop();
+            }
+            // 得到索引间距
+            res[i] = s.isEmpty() ? 0 : (s.peek() - i); 
+            // 将索引入栈，而不是元素
+            s.push(i); 
+        }
+        return res;
+    }
+}
