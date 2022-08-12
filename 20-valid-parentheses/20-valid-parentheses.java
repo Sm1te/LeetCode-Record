78ece07b87a4c8c3bd3ab4fafe77d0685d1c295a
Robin Li <liyijiannice@outlook.com>
2022-08-12 15:59:08 -0500

    Time: 2 ms (90.79%), Space: 42.1 MB (52.30%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack();
-        
+        Deque<Character> stack = new ArrayDeque();
+
         for(int i = 0; i < s.length(); i++){
-            char tmp = s.charAt(i);
-            if(tmp == '(')
+            char cur = s.charAt(i);
+            if(cur == '(')
                 stack.push(')');
-            else if(tmp == '[')
+            else if(cur == '[')
                 stack.push(']');
-            else if(tmp == '{')
+            else if(cur == '{')
                 stack.push('}');
-            else if(stack.isEmpty() || stack.pop() != tmp)
-                return false;
+            else{
+                if(stack.isEmpty() || stack.peek() != cur)
+                    return false;
+                stack.pop();
+            }
         }
-        
-        return stack.isEmpty();
+        return stack.size() == 0;
     }
 }
