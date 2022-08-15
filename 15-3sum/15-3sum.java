1bad9e95bb048b2b71e1102a972226052e693919
Robin Li <liyijiannice@outlook.com>
2022-08-15 18:12:27 -0500

    Time: 30 ms (71.85%), Space: 60.1 MB (31.30%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,50 +1,38 @@
 class Solution {
-    /*
-    1. 原数组一定要先排序
-    2. 如果开始有 > 0的作为起点了，就不可能有加起来 = 0 的sum了
-    3. 查重要在内部查重
-    4. res.add(Arrays.asList(xxx));
-    */
-    public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList();
-        if(nums.length < 3)
-            return res;
+    List<List<Integer>> res = new ArrayList();
 
-        Arrays.sort(nums);
-        
+    public List<List<Integer>> threeSum(int[] nums) {
         int n = nums.length;
-        for(int i = 0; i < n; i++){
-            //这部分是可以直接判断的
-            if(nums[i] > 0)
-                return res;
-            
-            //对i查重
+        Arrays.sort(nums);
+        for(int i = 0; i < n - 2; i++){
             if (i > 0 && nums[i] == nums[i - 1]) {
                 continue;
             }
-            
-            int left = i + 1;
-            int right = n - 1;
-            while(left < right){
-                int sum = nums[i] + nums[left] + nums[right];
-                
-                if(sum < 0){
-                    left++;
-                }else if(sum > 0){
-                    right--;
+            if(nums[i] > 0)
+                return res;
+
+            int l = i + 1, r = n - 1;
+            while(l < r){
+                int sum = nums[i] + nums[l] + nums[r];
+                if(sum > 0){
+                    r--;
+                }else if(sum < 0){
+                    l++;
                 }else{
-                    res.add(Arrays.asList(nums[i] , nums[left] , nums[right]));
-                    while(left < right && nums[left] == nums[left + 1])
-                        left++;
-                    while(left < right && nums[right] == nums[right - 1])
-                        right--;
-                    
-                    left++;
-                    right--;
+                    List<Integer> tmp = new ArrayList();
+                    tmp.add(nums[i]);
+                    tmp.add(nums[l]);
+                    tmp.add(nums[r]);
+                    res.add(new ArrayList(tmp));
+                    while(l < r && nums[l] == nums[l + 1])
+                        l++;
+                    while(l < r && nums[r] == nums[r - 1])
+                        r--;
+                    l++;
+                    r--;
                 }
             }
         }
-        
         return res;
     }
 }
