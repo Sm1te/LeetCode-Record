d92c6bad54d7381da99f29c2ef82af90d343cb69
Robin Li <liyijiannice@outlook.com>
2022-08-14 19:38:40 -0500

    Time: 75 ms (45.61%), Space: 53.9 MB (62.21%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,27 +1,30 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum = Arrays.stream(nums).sum();
-        if(sum % 2 != 0)
+        int sum = 0;
+        for(int i : nums)
+            sum += i;
+        if(sum % 2 == 1)
             return false;
         
-        int n = nums.length;
         int target = sum / 2;
-        boolean[][] dp = new boolean[n + 1][sum + 1];
-        
-        for(int i = 0; i <= n; i++)
+        boolean[][] dp = new boolean[nums.length + 1][target + 1];
+        for(int i = 0; i <= nums.length; i++){
             dp[i][0] = true;
-        
-        // for(int j = 0; j <= sum; j++)
-        //     dp[nums.length][j] = false;
-        
-        for(int i = n - 1; i >= 0; i--){
-            for(int j = 0; j <= target; j++){
-                if(j < nums[i])
-                    dp[i][j] = dp[i + 1][j];
-                else
-                    dp[i][j] = dp[i + 1][j - nums[i]] || dp[i + 1][j];
-            }
         }
-        return dp[0][target];
+
+        for(int i = 1; i <= nums.length; i++){
+            for(int j = 1; j <= target; j++){
+                if (j - nums[i - 1] < 0) {
+                    // 背包容量不足，不能装入第 i 个物品
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    // 装入或不装入背包
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                }
+            }
+            
+        }
+
+        return dp[nums.length][target];
     }
 }
