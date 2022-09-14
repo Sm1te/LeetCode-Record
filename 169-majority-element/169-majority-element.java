41f86cdf9d42c2f14b790200b4abaa9eb6cf9294
Robin Li <liyijiannice@outlook.com>
2022-09-13 20:30:10 -0500

    Time: 7 ms (43.90%), Space: 56.2 MB (44.57%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,19 +1,11 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        if(n == 1)
-            return nums[0];
-        
-        Arrays.sort(nums);
-        int count = 1;
-        for(int i = 1; i < n; i++){
-            while(i < n && nums[i] == nums[i - 1]){
-                count++;
-                i++;
-            }
-            if(count > n / 2)
-                return nums[i - 1];
+        int x = 0, votes = 0;
+        for(int num : nums){
+            if(votes == 0) 
+                x = num;
+            votes += num == x ? 1 : -1;
         }
-        return -1;
+        return x;
     }
 }
