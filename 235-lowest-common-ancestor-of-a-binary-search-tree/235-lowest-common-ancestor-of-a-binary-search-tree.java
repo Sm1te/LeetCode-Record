d927842d3d4c9c3180ef85d36025b2ce02fc1dba
Robin Li <liyijiannice@outlook.com>
2022-10-06 20:57:17 -0500

    Time: 20 ms (6.31%), Space: 50 MB (44.71%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -8,23 +8,40 @@
  * }
  */
 
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        // 保证 val1 较小，val2 较大
-        int val1 = Math.min(p.val, q.val);
-        int val2 = Math.max(p.val, q.val);
-        return find(root, val1, val2);
-    }
-    
-    public TreeNode find(TreeNode root, int val1, int val2){
+        int val1 = p.val, val2 = q.val;
         if(root == null)
             return null;
-        
-        if(root.val > val2){
-            return find(root.left, val1, val2);
-        }else if(root.val < val1){
-            return find(root.right, val1, val2);
+
+        if(val1 > val2){
+            int tmp = val1;
+            val1 = val2;
+            val2 = tmp;
         }
+
+        return dfs(root, val1, val2);
+    }
+
+    public TreeNode dfs(TreeNode root, int val1 , int val2){
+        if(root == null)
+            return null;
+
+        if(root.val > val2){
+            return dfs(root.left, val1, val2);
+        }else if(root.val < val1){
+            return dfs(root.right, val1, val2);
+        }
+
         return root;
     }
 }
